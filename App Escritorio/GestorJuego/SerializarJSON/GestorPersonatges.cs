5fbdcf1ac1a079d3b26871dc493eea1dda6a3153
Marc <55625687+Marc-Ferrer-Castillo@users.noreply.github.com>
2019-10-22 11:16:48 +0200

    form ayuda

---
+++ App Escritorio/GestorJuego/SerializarJSON/GestorPersonatges.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerializarJSON
+{
+    public partial class GestorPersonatges : Form
+    {
+        public GestorPersonatges()
+        {
+            InitializeComponent();
+        }
+    }
+}

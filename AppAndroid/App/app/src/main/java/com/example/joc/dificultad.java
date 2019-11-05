1fbee89c74b7a4cb099d6ec15974b1818643bd41
Marc <Marc-Ferrer-Castillo@users.noreply.github.com>
2019-11-05 17:36:46 +0100

    Acitividades creadas

---
+++ AppAndroid/App/app/src/main/java/com/example/joc/dificultad.java
@@ -0,0 +1,14 @@
+package com.example.joc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dificultad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dificultad);
+    }
+}

68606dd8a27a276809fdbe208c011d2afdd28048
ivancg86 <ivancg86@gmail.com>
2019-11-09 20:15:28 +0100

    Cambios en dificultad y resultado

--- AppAndroid/App/app/src/main/java/com/example/joc/dificultad.java
+++ AppAndroid/App/app/src/main/java/com/example/joc/dificultad.java
@@ -4,19 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Locale;
 
 public class dificultad extends AppCompatActivity {
 
-    /*0=CATALAN  1=CASTELLANO  2=INGLES*/
-    /*public static byte idioma;
-
-    private String catalan = "ca";
-    private String espanol = "es";
-    private String ingles  = "en";*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +21,39 @@ public class dificultad extends AppCompatActivity {
 
         Button facil = findViewById(R.id.facil);
         Button dificil = findViewById(R.id.dificil);
+        final TextView tiempo = findViewById(R.id.tiempo);
 
 
 
-       facil.setOnClickListener(new View.OnClickListener() {
-       @Override
-             public void onClick(View v) {
-                 Intent intent = new Intent(getApplicationContext(), contenido.class);
-                 // Inicia la actividad
-                 startActivity(intent);
-             }
-         });
-        dificil.setOnClickListener(new View.OnClickListener()
-
-            {
-                @Override
-                public void onClick (View v){
+        facil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), contenido.class);
                 // Inicia la actividad
                 startActivity(intent);
-                }
+            }
         });
+        dificil.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick (View v){
+                Intent intent = new Intent(getApplicationContext(), contenido.class);
+                // Inicia la actividad
+                startActivity(intent);
+            }
+        });
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                tiempo.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                // Inicia la actividad
+                startActivity(intent);
+            }
+        }.start();
     }
 }

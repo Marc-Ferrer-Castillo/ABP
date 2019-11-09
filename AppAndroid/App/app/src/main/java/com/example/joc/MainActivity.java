e77f1d52cfda375b46d519e44a35465d11024ac6
Marc <Marc-Ferrer-Castillo@users.noreply.github.com>
2019-11-09 01:07:03 +0100

    Solicitud de permisos de lectura

--- AppAndroid/App/app/src/main/java/com/example/joc/MainActivity.java
+++ AppAndroid/App/app/src/main/java/com/example/joc/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.joc;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    public static final int REQUEST_CODE  = 1;
 
     /*0=CATALAN  1=CASTELLANO  2=INGLES*/
     public static byte idioma;
@@ -35,6 +42,45 @@ public class MainActivity extends AppCompatActivity {
         Button contenido = findViewById(R.id.contenido);
         Button juego = findViewById(R.id.juego);
         Button resultado = findViewById(R.id.resultado);
+
+
+
+
+        // Controlem la versió d'Android que estem executant.
+        if (android.os.Build.VERSION.SDK_INT >= 23)
+        {
+            // Si no s'ha concedit el permís de lectura
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                    // Obre el menu perque es donguin permissos
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+
+                }
+                else {
+                    demanarPermisos();
+                }
+            }
+            // Si s' ha concedit el permís
+            else{
+
+            }
+        }
+        // Si executem una versió anterior a la versió Marshmallow (6.0),
+        // no cal demanar cap permís, i podem executar el nostre codi directament
+        else
+        {
+
+        }
+
+
 
         // Click en INICIAR
         iniciar.setOnClickListener(new View.OnClickListener() {
@@ -78,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /*BOTONES PROVISIONALES*/
+        /*-----------------------BOTONES PROVISIONALES------------------------------*/
 
         contenido.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        /*-----------------------BOTONES PROVISIONALES------------------------------*/
 
     }
 
@@ -122,7 +168,32 @@ public class MainActivity extends AppCompatActivity {
         recreate();
     }
 
+    // Cuando el usuario ha respondido a la solicitud de permisos en tiempo real
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE: {
+                // Si se han otorgado permisos
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
+
+                }
+                // Si no
+                else {
+                    demanarPermisos();
+                }
+                return;
+            }
+        }
+    }
+
+
+    public void demanarPermisos(){
+        // Demana permissos en temps d'execució
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE);
+
+    }
 
 
 }

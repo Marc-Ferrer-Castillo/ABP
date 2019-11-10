a9ee7f2a3296d018db176beba2c33c701615aa9a
Marc <Marc-Ferrer-Castillo@users.noreply.github.com>
2019-11-10 18:13:18 +0100

    ya cargan las preguntas y respuestas del juego

--- AppAndroid/App/app/src/main/java/com/example/joc/Juego.java
+++ AppAndroid/App/app/src/main/java/com/example/joc/Juego.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +22,21 @@ public class Juego extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
 
-        // Objeto GridView
+        // Objeto GridView donde iran las respuestas
         ListView gridRespuestas = findViewById(R.id.gridRespuestas);
 
+        // Objeto Textview donde ira la pregunta
+        TextView preguntaView = findViewById(R.id.pregunta);
+
+        // Contiene el string con la pregunta numPregunta de planetaMostrado
+        String pregunta = MainActivity.planetas.get(MainActivity.planetaMostrado).
+                getPreguntas().get(numPregunta).getPregunta();
+
+        // coloca la pregunta en el textView
+        preguntaView.setText(pregunta);
+
+
+        // Lista de respuestas de la pregunta
         final List<Respuesta> respuestas = MainActivity.planetas.get(MainActivity.planetaMostrado).
                 getPreguntas().get(numPregunta).getRespuestas();
 

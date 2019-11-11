422fb51548745f72c6e3fd31867f7f1359b953dc
ivancg86 <ivancg86@gmail.com>
2019-11-11 12:37:47 +0100

    Salir de Resultado

--- AppAndroid/App/app/src/main/java/com/example/joc/Resultado.java
+++ AppAndroid/App/app/src/main/java/com/example/joc/Resultado.java
@@ -3,7 +3,10 @@ package com.example.joc;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.FontResourcesParserCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Resultado extends AppCompatActivity {
@@ -19,8 +22,19 @@ public class Resultado extends AppCompatActivity {
 
 
         TextView aciertosView = findViewById(R.id.aciertos);
+        Button salir = findViewById(R.id.salir);
+        final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 
         aciertosView.setText("El numero de aciertos es de " + String.valueOf(aciertos) );
         aciertos = 0;
+
+        salir.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                // Inicia la actividad
+                startActivity(intent);
+            }
+        });
     }
 }

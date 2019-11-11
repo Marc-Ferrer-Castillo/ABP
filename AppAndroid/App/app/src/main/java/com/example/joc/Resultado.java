3934b7bb2969b2de177605f6163ce066f3b6aa3d
ivancg86 <ivancg86@gmail.com>
2019-11-11 13:35:27 +0100

    retoque activity resultado

--- AppAndroid/App/app/src/main/java/com/example/joc/Resultado.java
+++ AppAndroid/App/app/src/main/java/com/example/joc/Resultado.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Resultado extends AppCompatActivity {
@@ -22,7 +23,7 @@ public class Resultado extends AppCompatActivity {
 
 
         TextView aciertosView = findViewById(R.id.aciertos);
-        Button salir = findViewById(R.id.salir);
+        ImageButton salir = findViewById(R.id.inicio);
         final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 
         aciertosView.setText("El numero de aciertos es de " + String.valueOf(aciertos) );

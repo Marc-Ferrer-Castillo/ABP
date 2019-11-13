7294ced54780c23f2e7155ca7fd096b196767195
Marc <55625687+Marc-Ferrer-Castillo@users.noreply.github.com>
2019-11-13 11:35:11 +0100

    imports sin usar borrados

--- AppAndroid/App/app/src/main/java/com/example/joc/RespuestaAdapter.java
+++ AppAndroid/App/app/src/main/java/com/example/joc/RespuestaAdapter.java
@@ -1,18 +1,12 @@
 package com.example.joc;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-
-import java.io.File;
 import java.util.List;
 
 public class RespuestaAdapter extends ArrayAdapter {

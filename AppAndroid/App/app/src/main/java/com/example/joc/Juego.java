927c24cb20499fde149ace6ee9c66944d34d69ea
Marc <Marc-Ferrer-Castillo@users.noreply.github.com>
2019-11-20 20:18:18 +0100

    Comentarios en Juego.java

--- AppAndroid/App/app/src/main/java/com/example/joc/Juego.java
+++ AppAndroid/App/app/src/main/java/com/example/joc/Juego.java
@@ -21,24 +21,32 @@ import java.util.List;
 
 public class Juego extends AppCompatActivity {
 /*
- 1. Recibimos la primera pregunta a mostrar con las variables:
-    MainActivity.getPlanetaMostrado() y preguntaMostrada
+ 1. Recibimos la primera pregunta a mostrar a traves de las variables:
+    PlanetaMostrado y preguntaMostrada
 
- 2. Filtrar las preguntas y guardarlas en una lista nueva
+ 2. Se filtran las preguntas por dificultad con el metodo filtrarPreguntas y se guardan en preguntasFiltradas
 
- 3. Rellenar(); <-- Muestra la pregunta en el textview y sus respuestas en la gridview
+ 3. Se carga el contenido mediante el método cargarContenido()
 
  4. OnClick respuesta:
-    4.1 Si hay más preguntas se muestra la siguiente y llamamos a rellenar()
-    4.2 Si no hay más preguntas pasamos de planeta (si quedan)
+    Llama al método juego:
+        4.1 Si hay más preguntas se muestra la siguiente y llamamos a cargarContenido()
+        4.2 Si no hay más preguntas pasamos de planeta y si no quedan más planetas abrimos Resultado
 */
 
+    // Usado iniciar la actividad resultado
     private static final byte RESULTADO_ACTIVIDAD = 1;
+    // Guarda el número de aciertos
     private static int aciertos;
+    // Guarada la pregunta que debe mostrarse
     private static int preguntaMostrada = 0;
+    // Guarda el planeta que debe mostrarse
     private static int planetaMostrado;
+    // Se usa para filtrar las preguntas según la dificultad seleccionada
     private boolean dificultadSeleccionada;
+    // Guarda las preguntas filtradas
     private List<Pregunta> preguntasFiltradas = new ArrayList<Pregunta>();
+    // Guarda el último planeta según el idioma seleccionado (3 planetas por idioma)
     private int  maxPlaneta = 0;
 
 
@@ -47,52 +55,50 @@ public class Juego extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
 
-        final GridView gridrespuestas = (GridView) findViewById(R.id.gridrespuestas);
+        // GridView donde se mostrarán las respuestas de la pregunta
+        final GridView gridrespuestas = findViewById(R.id.gridrespuestas);
+        // TextView para mostrar el tiempo del contador
         final TextView contadorView = findViewById(R.id.tiempo);
 
+        // Recoge el intent con el planeta a mostrado, la dificultad y la pregunta a mostrar
         Intent intentDoble = getIntent();
         planetaMostrado = intentDoble.getIntExtra("planetaMostrado", 0);
         dificultadSeleccionada = intentDoble.getBooleanExtra("dificultad", false);
         preguntaMostrada = intentDoble.getIntExtra("preguntaMostrada", 0);
 
-        if (planetaMostrado >= 0 && planetaMostrado <= 2){
-            maxPlaneta = 2;
-        }
-        else if(planetaMostrado >= 3 && planetaMostrado <= 5){
-            maxPlaneta = 5;
-        }
-        else{
-            maxPlaneta = 8;
-        }
 
+        // Establece cuál es el último planeta según el idioma seleccionado
+        establecerMaxPlaneta();
+
+        // Contador del juego
         final CountDownTimer contador = new CountDownTimer(3000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 contadorView.setText("" + millisUntilFinished / 1000);
             }
 
+            // Cuando el contador llega a 0 se hace un click en una respuesta incorrecta
             @Override
             public void onFinish() {
 
+                // Si la primera respuesta es correcta se pulsa sobre la segunda
                 if (preguntasFiltradas.get(preguntaMostrada).getRespuestas().get(0).isEsCorrecta()){
 
                     gridrespuestas.performItemClick(gridrespuestas.getChildAt(1), 0, gridrespuestas.getItemIdAtPosition(1));
                 }
+                // Si la primera pregunta no es correcta se pulsa sobre esta
                 else{
                     gridrespuestas.performItemClick(gridrespuestas.getChildAt(0), 0, gridrespuestas.getItemIdAtPosition(0));
                 }
             }
         };
 
-        // Lista de planetas
-         List<Planeta> planetas = Importar.getPlanetas();
-
-
-
-        filtrarPreguntas( planetas.get(planetaMostrado).getPreguntas() );
+        // Obtiene las todas las preguntas y llama al método filtrarPreguntas
+        filtrarPreguntas( Importar.getPlanetas().get(planetaMostrado).getPreguntas() );
+        // Método que carga la pregunta y las respuestas en pantalla
         cargarContenido(gridrespuestas, contador);
 
-        // Imagen Salir
+        // Imagen Salir (casita)
         ImageView salir = findViewById(R.id.inicio);
 
         // Click en salir
@@ -109,22 +115,27 @@ public class Juego extends AppCompatActivity {
         });
 
 
-        final RelativeLayout juegoLayout;
-        juegoLayout = findViewById(R.id.juegoLayout);
+        // RelativeLayout usado para mostrar un SnackBar
+        final RelativeLayout juegoLayout = findViewById(R.id.juegoLayout);
 
-        // Al pulsar sobre un item del grid
+        // Al pulsar sobre un item del GridView
         gridrespuestas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
 
+                // El contador se cancela
                 contador.cancel();
 
+                // Se bloquea el click para evitar posibles errores
                 gridrespuestas.setEnabled(false);
+
+                // Se obtiene el LinearLayout correspondiente al item seleccioando
                 LinearLayout respuesta = (LinearLayout) view;
 
-                // Si la respuesta seleccionada es correcta
+                // Guarda TRUE si la respuesta seleccionada es correcta
                 boolean repuestaCorrecta = preguntasFiltradas.get(preguntaMostrada).getRespuestas().get(position).isEsCorrecta();
 
+                // Si la respuesta seleccionada es correcta
                 if(repuestaCorrecta ){
 
                     // Pone el fondo de la respuesta en verde
@@ -135,14 +146,16 @@ public class Juego extends AppCompatActivity {
                     // Pone el fondo de la respuesta pulsada en rojo
                     respuesta.getChildAt(0).setBackgroundColor(Color.parseColor("#5e3535"));
 
-                    // Por cada view dentro de la gridview
+                    // Elemento parent casteado a GridView para buscar entre items
                     GridView grid = (GridView) parent;
+
+                    // Recorre las respuestas de la grid en busca de la respuesta correcta
                     for (int i = 0 ; i < grid.getChildCount() ; i++){
 
-                        // Coge cada elemento dentro de parent > LinearLayour > Textview
+                        // Cada TextView de las views de la gridView
                         TextView text = (TextView)((LinearLayout)grid.getChildAt(i)).getChildAt(0);
 
-                        // Si la pregunta es correcta
+                        // Si la respuesta es correcta
                         if(preguntasFiltradas.get(preguntaMostrada).getRespuestas().get(i).isEsCorrecta()){
 
                             // Cambia el fondo de color verde
@@ -151,6 +164,7 @@ public class Juego extends AppCompatActivity {
                     }
                 }
 
+                // Método juego que decide si pasar a la siguiente pregunta o pasar planeta
                 juego(position , juegoLayout, gridrespuestas, contador);
 
             }
@@ -158,14 +172,48 @@ public class Juego extends AppCompatActivity {
 
     }
 
+    // Según el planeta seleccionado tendremos un planeta máximo distinto
+        // PLANETAS CATALAN: 0,1,2
+        // PLANETAS ESPAÑOL: 3,4,5
+        // PLANETAS INGLÉS:  6,7,8
+    private void establecerMaxPlaneta() {
+
+
+        // Si el idioma seleccionado es CATALÁN
+        if (planetaMostrado >= 0 && planetaMostrado <= 2){
+            // El último planeta será el 2
+            maxPlaneta = 2;
+        }
+        // Si el idioma seleccionado es ESPAÑOL
+        else if(planetaMostrado >= 3 && planetaMostrado <= 5){
+            // El último planeta será el 5
+            maxPlaneta = 5;
+        }
+        // Si el idioma seleccionado es INGLÉS
+        else{
+            // El último planeta será el 8
+            maxPlaneta = 8;
+        }
+    }
+
+    // juego: Si quedan más preguntas pasa a la siguiente, sino se pasa al siguiente planeta, si no, a la actividad Resultado
+    // PARÁMETROS:
+        // position:       Índice de la respuesta pulsada
+        // juegoLayout:    Layout donde se muestra el SnackBar
+        // gridRespuestas: GridView donde se muestran las respuestas
+        // contador:       Contador del juego
     private void juego(int position, RelativeLayout juegoLayout, final GridView gridRespuestas, final CountDownTimer contador ) {
 
-        // Siguiente Pregunta
+        // Si quedan más preguntas por mostrar
         if ( preguntaMostrada < preguntasFiltradas.size() - 1 ){
 
+            // Si se ha acertado la respuesta
             if (preguntasFiltradas.get(preguntaMostrada).getRespuestas().get(position).isEsCorrecta()) {
+
+                // Suma un acierto
                 Resultado.setAciertos(aciertos++);
 
+                // Método que muestra un snackbar
                 mostrarSnackBar(juegoLayout);
 
                 // Pausa antes de pasar a la siguiente pregunta
@@ -176,9 +224,10 @@ public class Juego extends AppCompatActivity {
                         preguntaMostrada++;
                         cargarContenido(gridRespuestas, contador);
                     }
-                }, 1000);   //2 seconds
+                }, 2000);   //2 seconds
 
             }
+            // Si no se ha acertado la respuesta
             else{
                 // Pausa antes de pasar a la siguiente pregunta
                 Handler handler = new Handler();
@@ -188,45 +237,54 @@ public class Juego extends AppCompatActivity {
                         preguntaMostrada++;
                         cargarContenido(gridRespuestas, contador);
                     }
-                }, 1000);   //2 seconds
+                }, 2000);   //2 seconds
             }
         }
-        // Si no quedan mas preguntas
+        // Si no quedan más preguntas por mostrar
         else{
 
+            // Si se ha acertado la respuesta
             if (preguntasFiltradas.get(preguntaMostrada).getRespuestas().get(position).isEsCorrecta()) {
 
+                // Incrementa el número de aciertos
                 Resultado.setAciertos(aciertos++);
+                // Muestra un snackbar
                 mostrarSnackBar(juegoLayout);
             }
 
+            // Incrementa el planeta
             planetaMostrado++;
 
-            // Siguiente planeta
+            // Si quedan más planetas por mostrar
             if ( planetaMostrado <= maxPlaneta ){
 
                 // Devuelve RESULT OK a la clase Dificultad
                 setResult(Contenido.RESULT_FIRST_USER);
 
+                // Metodo para que al volver empiece por la pregunta 0 del siguiente planeta
                 reiniciarPreguntas();
 
                 //Cierra esta actividad
                 finish();
             }
-            // Pantalla resultado
+            // Muestra la Pantalla Resultado
             else{
+
+                // Metodo para que al volver empiece por la pregunta 0 del siguiente planeta
                 reiniciarPreguntas();
 
+                //Intent para pasar el planetaMostrado
                 Intent intentResultado = new Intent(getApplicationContext(), Resultado.class);
-
                 intentResultado.putExtra("planetaMostrado", planetaMostrado);
 
+                // Se abre Resultado
                 startActivityForResult(intentResultado, RESULTADO_ACTIVIDAD );
             }
         }
 
     }
 
+    // Método para mostrar un Snackbar al acertar una respuesta
     private void mostrarSnackBar(RelativeLayout juegoLayout) {
         // Muestra un snackbar
 
@@ -261,25 +319,37 @@ public class Juego extends AppCompatActivity {
         snackbar.show();
     }
 
+    // carga la pregunta y las respuestas en la gridView
+    // PARAMETROS:
+        // gridRespuestas: GridView donde se muestran las respuestas
+        // contador:       Contador del juego
     private void cargarContenido(final GridView gridRespuestas, CountDownTimer contador) {
 
+        // Lista de respuestas de la pregunta actual
         List<Respuesta> respuestas = preguntasFiltradas.get(preguntaMostrada).getRespuestas();
 
+        // TextView donde va la pregunta
         TextView viewPregunta = findViewById(R.id.pregunta);
 
+        // Coloca la pregunta en el TextView
         viewPregunta.setText(preguntasFiltradas.get(preguntaMostrada).getPregunta());
 
         //Instancia nuestro adaptador personalizado
         Adaptador adaptador = new Adaptador(this, respuestas);
 
+        // Establece el adaptor al GridView
         gridRespuestas.setAdapter(adaptador);
 
+        // Inicia el contador
         contador.start();
     }
 
+    // Método para filtrar las preguntas según la dificultad seleccioanda
+    // PARAMETROS
+        // preguntas: Lista con todas las preguntas
     private void filtrarPreguntas(List<Pregunta> preguntas) {
 
-        // Dificultad de juego: Facil
+        // Si la dificultad seleccionada es fácil
         if (!dificultadSeleccionada){
 
             // Recorre la lista de preguntas
@@ -325,19 +395,19 @@ public class Juego extends AppCompatActivity {
                 // Devuelve RESULT OK a la clase Dificultad
                 setResult(Contenido.RESULT_OK);
 
-
+                // Reinicia los aciertos
                 aciertos = 0;
+
+                // Metodo para reinciar las preguntas y empezar por la primera al volver
                 reiniciarPreguntas();
 
-
-                // Cerramos esta actividad también
+                // Cerramos esta actividad
                 finish();
             }
-
-
         }
     }
 
+    // Se vacía la lista de preguntas filtradas y se pone a 0 la pregunta a mostrar
     private void reiniciarPreguntas() {
         preguntasFiltradas.clear();
         preguntaMostrada = 0;
